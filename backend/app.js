662aa4f2a678dd82080d226293ad486fd258dabe
Shriyatotlod <shriyatotlod2001@gmail.com>
2024-06-15 03:21:02 +0530

    new url added in cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(cors({
     Credentials:true,
     methods:["GET","POST","PUT","DELETE"],
-    origin:["https://portfolio-jdbkcoaa5-saikirannarawad.vercel.app","https://jocular-wisp-d01861.netlify.app", "http://localhost:3000","https://portfolio-saikirannarawad.vercel.app"],
+    origin:["https://portfolio-jdbkcoaa5-saikirannarawad.vercel.app", "https://portfolio24-frontend.vercel.app/","https://jocular-wisp-d01861.netlify.app", "http://localhost:3000","https://portfolio-saikirannarawad.vercel.app"],
 }));
 
 app.get("/", (req, res, next) => {
